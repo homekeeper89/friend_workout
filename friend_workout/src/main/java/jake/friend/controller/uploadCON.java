78cf22be9d193ffbfe85e75b4c7814b0d64fdfcd
DESKTOP-5FNMD68\Jake-gram <homekeerp>
2018-06-14 16:17:14 +0900

    드디어 섬네일 이미지 불러오기 성공

--- friend_workout/src/main/java/jake/friend/controller/uploadCON.java
+++ friend_workout/src/main/java/jake/friend/controller/uploadCON.java
@@ -1,23 +1,32 @@
 package jake.friend.controller;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.UUID;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import jake.friend.domain.bbsVO;
 import jake.friend.persistence.bbsDAOImpl;
 import jake.friend.persistence.bbsService;
+import jake.friend.util.MediaUtils;
 import jake.friend.util.uploadFileUtils;
 
 @Controller
@@ -44,6 +53,50 @@ public class uploadCON {
 	@RequestMapping(value = "/files", method = RequestMethod.GET)
 	public String uploadForm() {
 		return "getfiles";
+		
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="/uploadajax", method=RequestMethod.POST)
+	public String uploadAjax(MultipartFile file) throws Exception {
+		
+		return "/2018/06/14/s_a0246319-fb5b-4c24-8e94-9f66995f06d5_KakaoTalk_20180614_132151577.jpg";
+		
+	}
+	
+	@ResponseBody
+	@RequestMapping("/displayfile")
+	public ResponseEntity<byte[]> displayFile(String fileName)throws Exception{
+		InputStream in = null;
+		ResponseEntity<byte[]> entity = null;
+		
+		logger.info("FILE NAME : " + fileName);
+		try {
+			String formatName = fileName.substring(fileName.lastIndexOf(".")+1);
+			MediaType mType = MediaUtils.getMediaType(formatName);
+			HttpHeaders headers = new HttpHeaders();
+			in = new FileInputStream(uploadPath+fileName);
+			
+			//step: change HttpHeader ContentType
+			if(mType != null) {
+				//image file(show image)
+				headers.setContentType(mType);
+			}else {
+				//another format file(download file)
+				fileName = fileName.substring(fileName.indexOf("_")+1);//original file Name
+				headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+				headers.add("Content-Disposition", "attachment; filename=\"" + new String(fileName.getBytes("UTF-8"), "ISO-8859-1")+"\""); 
+			}
+			
+			entity = new ResponseEntity<byte[]>(IOUtils.toByteArray(in), headers, HttpStatus.CREATED);
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+			entity = new ResponseEntity<byte[]>(HttpStatus.BAD_REQUEST);
+		}finally {
+			in.close();
+		}
+			return entity;
 		
 	}
 }
--- friend_workout/src/main/webapp/WEB-INF/views/getfiles.jsp
+++ friend_workout/src/main/webapp/WEB-INF/views/getfiles.jsp
@@ -4,15 +4,32 @@
 <!DOCTYPE>
 <html>
 <head>
+<script src = "https://code.jquery.com/jquery-3.3.1.min.js"></script>
 <meta http-equiv="Content-Type" content="text/html; charset=EUC-KR">
 <title>Insert title here</title>
 </head>
 <body>
 <h3> hello</h3>
-<%-- <img src = "<c:url value = "\img\9b857fc4-f716-4cd1-a48f-d2fdc223434c_photo_2018-06-10_17-11-47.jpg"/>"/> --%>
-<img src = "${pageContext.request.contextPath}\G:\friend\workingout\2018\06\11\s_9b857fc4-f716-4cd1-a48f-d2fdc223434c_photo_2018-06-10_17-11-47.jpg">	
-	<%-- <img src="<c:url value="\img\${s_9b857fc4-f716-4cd1-a48f-d2fdc223434c_photo_2018-06-10_17-11-47.jpg}"/>"" width="100" height="60"/> --%>
+<div class="textView1"></div>
+<script>
+ $(document).ready(function() {
+	$.ajax({
+	url: '/uploadajax',
+	processData: false,
+	contentType: false,
+	type: 'POST',
+	success: function(data){
+		alert(data);
+		var str = "";
+		str = "<div>" +
+		"<img src = 'displayfile?fileName=" + data + "/'>" + data + "</div>";
+		$(".textView1").append(str);
+		
+			}
+		}
+	);
 
-	
+});
+</script>
 </body>
 </html>
